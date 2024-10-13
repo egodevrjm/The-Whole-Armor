674778244b0d6eaa7ca288643ed0454bf8d85679
Ryan Morrison <ryanmorrison@gmail.com>
2024-10-13 23:30:51 +0100

    Initial Commit

---
+++ The Whole Armor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Whole Armor
+//
+//  Created by Ryan Morrison on 10/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Whole Armor/The_Whole_ArmorApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Whole_ArmorApp.swift
+//  The Whole Armor
+//
+//  Created by Ryan Morrison on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Whole_ArmorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
